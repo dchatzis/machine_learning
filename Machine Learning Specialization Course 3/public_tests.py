010cb7cd7466d80e87799b369fdcd669c93300a5
Dimitris Chatzigeorgiou <dchatzig@gmail.com>
2025-06-20 13:55:06 -0700

    unsupervised learning and recommenders

--- Machine Learning Specialization Course 3/public_tests.py
+++ Machine Learning Specialization Course 3/public_tests.py
@@ -1,63 +1,78 @@
 import numpy as np
 
-def compute_centroids_test(target):
-    # With 3 centroids
-    X = np.array([[-1, -1], [-1.5, -1.5], [-1.5, 1],
-                  [-1, 1.5], [2.5, 1.5], [-1.1, -1.7], [-1.6, 1.2]])
-    idx = np.array([1, 1, 1, 0, 0, 0, 2])
-    K = 3
-    centroids = target(X, idx, K)
-    expected_centroids = np.array([[0.13333333,  0.43333333],
-                                   [-1.33333333, -0.5      ],
-                                   [-1.6,        1.2       ]])
+def test_cofi_cost_func(target):
+    num_users_r = 4
+    num_movies_r = 5 
+    num_features_r = 3
+
+    X_r = np.ones((num_movies_r, num_features_r))
+    W_r = np.ones((num_users_r, num_features_r))
+    b_r = np.zeros((1, num_users_r))
+    Y_r = np.zeros((num_movies_r, num_users_r))
+    R_r = np.zeros((num_movies_r, num_users_r))
     
-    assert type(centroids) == np.ndarray, "Wrong type"
-    assert centroids.shape == (K, X.shape[1]), f"Wrong shape. Expected: {centroids.shape} got: {(K, X.shape[1])}"
-    assert np.allclose(centroids, expected_centroids), f"Wrong values. Expected: {expected_centroids}, got: {centroids}"
+    J = target(X_r, W_r, b_r, Y_r, R_r, 2);
+    assert not np.isclose(J, 13.5), f"Wrong value. Got {J}. Did you multiply the regularization term by lambda_?"
+    assert np.isclose(J, 27), f"Wrong value. Expected {27}, got {J}. Check the regularization term"
     
-    X = np.array([[2, 2.5], [2.5, 2.5], [-1.5, -1.5],
-                  [2, 2], [-1.5, -1], [-1, -1]])
-    idx = np.array([0, 0, 1, 0, 1, 1])
-    K = 2
-    centroids = target(X, idx, K)
-    expected_centroids = np.array([[[ 2.16666667,  2.33333333],
-                                    [-1.33333333, -1.16666667]]])
     
-    assert type(centroids) == np.ndarray, "Wrong type"
-    assert centroids.shape == (K, X.shape[1]), f"Wrong shape. Expected: {(len(X),)} got: {idx.shape}"
-    assert np.allclose(centroids, expected_centroids), f"Wrong values. Expected: {expected_centroids}, got: {centroids}"
+    X_r = np.ones((num_movies_r, num_features_r))
+    W_r = np.ones((num_users_r, num_features_r))
+    b_r = np.ones((1, num_users_r))
+    Y_r = np.ones((num_movies_r, num_users_r))
+    R_r = np.ones((num_movies_r, num_users_r))
+
+    # Evaluate cost function
+    J = target(X_r, W_r, b_r, Y_r, R_r, 0);
     
-    print("\033[92mAll tests passed!")
+    assert np.isclose(J, 90), f"Wrong value. Expected {90}, got {J}. Check the term without the regularization"
     
-def find_closest_centroids_test(target):
-    # With 2 centroids
-    X = np.array([[-1, -1], [-1.5, -1.5], [-1.5, -1],
-                  [2, 2],[2.5, 2.5],[2, 2.5]])
-    initial_centroids = np.array([[-1, -1], [2, 2]])
-    idx = target(X, initial_centroids)
     
-    assert type(idx) == np.ndarray, "Wrong type"
-    assert idx.shape == (len(X),), f"Wrong shape. Expected: {(len(X),)} got: {idx.shape}"
-    assert np.allclose(idx, [0, 0, 0, 1, 1, 1]), "Wrong values"
+    X_r = np.ones((num_movies_r, num_features_r))
+    W_r = np.ones((num_users_r, num_features_r))
+    b_r = np.ones((1, num_users_r))
+    Y_r = np.zeros((num_movies_r, num_users_r))
+    R_r = np.ones((num_movies_r, num_users_r))
+
+    # Evaluate cost function
+    J = target(X_r, W_r, b_r, Y_r, R_r, 0);
     
-    # With 3 centroids
-    X = np.array([[-1, -1], [-1.5, -1.5], [-1.5, 1],
-                  [-1, 1.5], [2.5, 1.5], [2, 2]])
-    initial_centroids = np.array([[2.5, 2], [-1, -1], [-1.5, 1.]])
-    idx = target(X, initial_centroids)
+    assert np.isclose(J, 160), f"Wrong value. Expected {160}, got {J}. Check the term without the regularization"
     
-    assert type(idx) == np.ndarray, "Wrong type"
-    assert idx.shape == (len(X),), f"Wrong shape. Expected: {(len(X),)} got: {idx.shape}"
-    assert np.allclose(idx, [1, 1, 2, 2, 0, 0]), f"Wrong values. Expected {[2, 2, 0, 0, 1, 1]}, got: {idx}"
+    X_r = np.ones((num_movies_r, num_features_r))
+    W_r = np.ones((num_users_r, num_features_r))
+    b_r = np.ones((1, num_users_r))
+    Y_r = np.ones((num_movies_r, num_users_r))
+    R_r = np.ones((num_movies_r, num_users_r))
+
+    # Evaluate cost function
+    J = target(X_r, W_r, b_r, Y_r, R_r, 1);
     
-    # With 3 centroids
-    X = np.array([[-1, -1], [-1.5, -1.5], [-1.5, 1],
-                  [-1, 1.5], [2.5, 1.5], [-1.1, -1.7], [-1.6, 1.2]])
-    initial_centroids = np.array([[2.5, 2], [-1, -1], [-1.5, 1.]])
-    idx = target(X, initial_centroids)
+    assert np.isclose(J, 103.5), f"Wrong value. Expected {103.5}, got {J}. Check the term without the regularization"
     
-    assert type(idx) == np.ndarray, "Wrong type"
-    assert idx.shape == (len(X),), f"Wrong shape. Expected: {(len(X),)} got: {idx.shape}"
-    assert np.allclose(idx, [1, 1, 2, 2, 0, 1, 2]), f"Wrong values. Expected {[2, 2, 0, 0, 1, 1]}, got: {idx}"
+    num_users_r = 3
+    num_movies_r = 4 
+    num_features_r = 4
     
-    print("\033[92mAll tests passed!")
+    #np.random.seed(247)
+    X_r = np.array([[0.36618032, 0.9075415,  0.8310605,  0.08590986],
+                     [0.62634721, 0.38234325, 0.85624346, 0.55183039],
+                     [0.77458727, 0.35704147, 0.31003294, 0.20100006],
+                     [0.34420469, 0.46103436, 0.88638208, 0.36175401]])#np.random.rand(num_movies_r, num_features_r)
+    W_r = np.array([[0.04786854, 0.61504665, 0.06633146, 0.38298908], 
+                    [0.16515965, 0.22320207, 0.89826005, 0.14373251], 
+                    [0.1274051 , 0.22757303, 0.96865613, 0.70741111]])#np.random.rand(num_users_r, num_features_r)
+    b_r = np.array([[0.14246472, 0.30110933, 0.56141144]])#np.random.rand(1, num_users_r)
+    Y_r = np.array([[0.20651685, 0.60767914, 0.86344527], 
+                    [0.82665019, 0.00944765, 0.4376798 ], 
+                    [0.81623732, 0.26776794, 0.03757507], 
+                    [0.37232161, 0.19890823, 0.13026598]])#np.random.rand(num_movies_r, num_users_r)
+    R_r = np.array([[1, 0, 1], [1, 0, 0], [1, 0, 0], [0, 1, 0]])#(np.random.rand(num_movies_r, num_users_r) > 0.4) * 1
+
+    # Evaluate cost function
+    J = target(X_r, W_r, b_r, Y_r, R_r, 3);
+    
+    assert np.isclose(J, 13.621929978531858, atol=1e-8), f"Wrong value. Expected {13.621929978531858}, got {J}."
+    
+    print('\033[92mAll tests passed!')
+    
